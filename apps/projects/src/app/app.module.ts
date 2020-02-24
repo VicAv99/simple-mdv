de263e0e31f877c94f322eeaa6710d43734332be
Victor Avila <victor.avila@venturplex.com>
2020-02-24 10:23:16 -0700

    feat: setup styling

--- apps/projects/src/app/app.module.ts
+++ apps/projects/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { CoreDataModule } from '@workspace/core-data';
+import { MaterialModule } from '@workspace/material';
+import { UiToolbarModule } from '@workspace/ui-toolbar';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreDataModule,
+    MaterialModule,
+    UiToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
