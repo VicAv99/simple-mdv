0804d30e1ce702eda5871f823dfbde8b8cd4ffdf
Victor Avila <victor.avila@venturplex.com>
2020-02-24 08:47:24 -0700

    chore: initial generation

---
+++ apps/projects/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'projects',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/projects',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
